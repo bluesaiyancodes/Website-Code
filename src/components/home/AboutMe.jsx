f11cec575fa4f0e76afac49022d83127938b0965
BlueSaiyan <blue.bishal@gmail.com>
2022-08-11 02:22:03 +0900

    re-edits

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -60,7 +60,7 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
                 role="button"
                 aria-label="Resume/CV"
               >
-                Curriculum vitae
+                Curriculum Vitae
               </a>
             </p>
           )}
